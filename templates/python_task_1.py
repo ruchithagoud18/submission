4635d9f7798e17834180a1f0453225aff68c53a4
ruchithagoud18 <153372155+ruchithagoud18@users.noreply.github.com>
2023-12-09 21:52:43 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,8 +13,8 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
-
-    return df
+    matrix = df.pivot(index='id_1', columns='id_2', values='car')
+    return matrix
 
 
 def get_type_count(df)->dict:
@@ -28,8 +28,8 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
-
-    return dict()
+    type_counts = df['car'].value_counts().to_dict()
+    return type_counts
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +43,8 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
-
-    return list()
+     bus_indexes = df[df['car'] == 'bus'].index[df['bus'] > 2 * df['bus'].mean()].tolist()
+    return bus_indexes
 
 
 def filter_routes(df)->list:
@@ -58,8 +58,10 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+    avg_truck_values = df.groupby('route')['truck'].mean()
+    selected_routes = avg_truck_values[avg_truck_values > 7].index.tolist()
+    return selected_routes
 
-    return list()
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,8 +75,9 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+    modified_matrix = matrix.applymap(lambda x: x * 2 if x % 2 == 0 else x)
+    return modified_matrix
 
-    return matrix
 
 
 def time_check(df)->pd.Series:
@@ -88,5 +91,7 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    completeness_check = df.groupby(['id', 'id_2'])['timestamp'].agg(lambda x: (x.max() - x.min()) >= pd.Timedelta(days=7) and (x.max().hour - x.min().hour) >= 24)
+    return completeness_check
 
-    return pd.Series()
+    
