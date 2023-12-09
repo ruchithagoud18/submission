7dc25178d8bf8485312ce8da333861c3707eb799
ruchithagoud18 <153372155+ruchithagoud18@users.noreply.github.com>
2023-12-09 22:01:24 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -12,8 +12,8 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
-
-    return df
+    distance_matrix = df.pivot(index='id_start', columns='id_end', values='distance')
+    return distance_matrix
 
 
 def unroll_distance_matrix(df)->pd.DataFrame():
@@ -27,8 +27,8 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
-
-    return df
+    unrolled_df = df.stack().reset_index(name='distance').rename(columns={'level_0': 'id_start', 'level_1': 'id_end'})
+    return unrolled_df
 
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
@@ -44,9 +44,13 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
+     reference_avg_distance = df[df['id_start'] == reference_id]['distance'].mean()
+    threshold = 0.1 * reference_avg_distance
 
-    return df
+    selected_ids = df.groupby('id_start')['distance'].mean()[abs(df.groupby('id_start')['distance'].mean() - reference_avg_distance) <= threshold].index
 
+    result_df = df[df['id_start'].isin(selected_ids)]
+    return result_df
 
 def calculate_toll_rate(df)->pd.DataFrame():
     """
@@ -59,7 +63,9 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
-
+    toll_rates = {'car': 0.05, 'bus': 0.1, 'truck': 0.2}  # Adjust rates as needed
+    df['toll_rate'] = df['vehicle_type'].map(toll_rates)
+    df['total_toll'] = df['distance'] * df['toll_rate']
     return df
 
 
@@ -74,5 +80,17 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+    df['hour'] = df['timestamp'].dt.hour
+
+
+    time_rates = {
+        (0, 6): 0.1,
+        (6, 12): 0.2,
+        (12, 18): 0.15,
+        (18, 24): 0.1
+    }
+
+    df['time_rate'] = pd.cut(df['hour'], bins=[0, 6, 12, 18, 24], labels=False).map(time_rates)
+    df['total_toll'] = df['distance'] * df['time_rate']
 
     return df
